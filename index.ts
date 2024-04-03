081759b494677ceafe4d816e37f3c7ef9d726208
Tanzeel <tanzeel498@gmail.com>
2024-04-03 22:32:56 +0500

    add logTodo function and add type annotation to it

--- index.ts
+++ index.ts
@@ -9,15 +9,19 @@ interface Todo {
 }
 
 axios.get(url).then((response) => {
-  const todo = response.data;
+  const todo = response.data as Todo;
 
-  const ID = todo.ID;
-  const title = todo.Title;
-  const finished = todo.finished;
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
 
-  console.log(`
-    The Todo with ID: ${ID}
-    Has a title of: ${title}
-    Is it finished? ${finished}
-  `);
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+`);
+};
