92f1cab7f007dca7bc45497e1080162c6196ac31
Tanzeel <tanzeel498@gmail.com>
2024-04-03 21:47:59 +0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response) => {
+  const todo = response.data;
+
+  const ID = todo.ID;
+  const title = todo.Title;
+  const finished = todo.finished;
+
+  console.log(`
+    The Todo with ID: ${ID}
+    Has a title of: ${title}
+    Is it finished? ${finished}
+  `);
+});
